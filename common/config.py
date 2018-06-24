859cda75c31c8279b3b921f742da871ae8d8e5bb
ouyangbro <xihuanjianguo@gmail.com>
2018-06-24 22:30:22 +0800

    init v1...

---
+++ common/config.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from common.constant import *
+
+run_venv = 1
+
+if run_venv == RUN_EVEN_TEST:
+    pass
+else:
+    pass
+
+DOC_DIR = "docs/"
+DOC_TEMPLATE_DIR = "doc_templates/"
---
+++ myutil/qiniu.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+
+access_key = "e2wVsnCxQET9faS1ql4Cen7RuEfsPm5_g8feztX0"
+securt_key = "3l4xuLswrTU0fnyvHbHtnp0HY5BtmaMpvmFv7TUl"
+
+
